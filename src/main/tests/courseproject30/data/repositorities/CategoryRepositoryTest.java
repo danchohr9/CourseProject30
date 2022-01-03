56f4f09389419f5aeaf1c80b25aa0a702fe0fae9
Philip <office@blublog.info>
2022-01-03 12:33:55 +0200

    Category repository test

---
+++ src/main/tests/courseproject30/data/repositorities/CategoryRepositoryTest.java
@@ -0,0 +1,81 @@
+package bg.tu_varna.sit.courseproject30.data.repositorities;
+
+import bg.tu_varna.sit.courseproject30.application.HelloApplication;
+import bg.tu_varna.sit.courseproject30.common.Constants;
+import bg.tu_varna.sit.courseproject30.data.entities.Category;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CategoryRepositoryTest {
+    private static CategoryRepository repo;
+
+    @BeforeAll
+    public static void setup() {
+        PropertyConfigurator.configure(HelloApplication.class.getResource(Constants.Configurations.LOG4J_PROPERTIES));
+        repo = CategoryRepository.getInstance();
+    }
+
+    @Test
+    @Order(1)
+    void getInstance() {
+        CategoryRepository Repository = new CategoryRepository();
+        assertEquals(Repository.getClass(),repo.getClass());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Testing if creating a category works.")
+    void save() {
+        Category category = new Category();
+        category.setDescription("test");
+        category.setName("test");
+        repo.save(category);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Testing if editing a category works.")
+    void update() {
+        Category category = new Category();
+        category.setId((long) repo.latestInsertedId());
+        category.setName("tests");
+        repo.update(category);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Testing if finding a category by id works.")
+    void getByIg() {
+        Category category = repo.findById((long) repo.latestInsertedId());
+        assertEquals("tests", category.getName());
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Testing if finding all categories works.")
+    void getAll() {
+        List<Category> categories = repo.getAll();
+        boolean recordFound = false;
+
+        for (int i = 0; i < categories.size(); i++) {
+            if(Objects.equals(categories.get(i).getName(), "tests")){
+                recordFound = true;
+            }
+        }
+        assertTrue(recordFound);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Testing if deleting a category works.")
+    void delete() {
+        Category category = new Category();
+        category.setId((long) repo.latestInsertedId());
+        repo.delete(category);
+    }
+}
